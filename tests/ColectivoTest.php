2b2db7f193905b3bbfcb2735e5949a9215f4fed6
thelmamedran <thelma.medran@gmail.com>
2023-09-02 16:30:38 -0300

    test viaje plus
    test viaje plus

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -17,15 +17,23 @@ class ColectivoTest extends TestCase {
         $nuevo_saldo = $tarjeta->obtenerSaldo();
         $this->assertEquals($nuevo_saldo, 150);
 
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $this->assertTrue($boleto->puede_viajar);
+        $boleto = $colectivo->pagarCon($tarjeta);
+        $this->assertTrue($boleto->puede_viajar);
+        $this->assertEquals(-210, $boleto->saldo_restante);
+
         $this->assertTrue($tarjeta->cargarSaldo(4000));
-        $this->assertFalse($tarjeta->cargarSaldo(4000));
         $nuevo_saldo = $tarjeta->obtenerSaldo();
-        $this->assertEquals($nuevo_saldo, 4150);
+        $this->assertEquals($nuevo_saldo, 3790);
 
         $boleto = $colectivo->pagarCon($tarjeta);
 
         $this->assertInstanceOf(Boleto::class, $boleto);
-        $this->assertEquals(4030, $boleto->saldo_restante);
+        $this->assertEquals(3670, $boleto->saldo_restante);
         $this->assertTrue($boleto->puede_viajar);
+
+
     }
 }
