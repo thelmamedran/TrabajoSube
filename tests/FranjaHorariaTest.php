32277221ce5f319269c470da5e332b3ef931604f
valennat <valentinanatale434@gmail.com>
2023-10-15 15:36:34 -0300

    cambios en franja horaira test

--- tests/FranjaHorariaTest.php
+++ tests/FranjaHorariaTest.php
@@ -6,20 +6,27 @@ class FranjaHorariaTest extends TestCase {
     public function testFranquiciasValidas() {
         $colectivo = new Colectivo(132);
 
-        // Verificamos que las franquicias sean válidas de lunes a viernes de 6 a 22
-        $this->assertTrue($colectivo->esHoraValida(new \DateTime('2023-10-09 06:00:00'))); // Lunes a las 6:00 AM
-        $this->assertTrue($colectivo->esHoraValida(new \DateTime('2023-10-09 21:59:00'))); // Lunes a las 9:59 PM
+        // Verificar que las franquicias sean válidas en la franja horaria
+        $tarjeta = new Tarjeta();
+        $this->assertTrue($tarjeta->esHoraValida(new \DateTime('2023-10-09 06:00:00'))); 
+        $this->assertTrue($tarjeta->esHoraValida(new \DateTime('2023-10-09 21:59:00'))); 
 
-        // Verificamos que las franquicias no sean válidas fuera de la franja horaria
-        $this->assertFalse($colectivo->esHoraValida(new \DateTime('2023-10-09 05:59:00'))); // Lunes a las 5:59 AM
-        $this->assertFalse($colectivo->esHoraValida(new \DateTime('2023-10-09 22:01:00'))); // Lunes a las 10:01 PM
+        // Verificar que las franquicias no sean válidas fuera de la franja horaria
+        $this->assertFalse($tarjeta->esHoraValida(new \DateTime('2023-10-09 05:59:00')));  
+        $this->assertFalse($tarjeta->esHoraValida(new \DateTime('2023-10-09 22:01:00'))); 
 
-        // Verificar que después de la franja horaria a los gratuitos y parciales se les cobra la tarifa normal
-        $colectivoInterurbano = new ColectivoInterurbano(132);
+        // Verificar que después de la franja horaria a las franquicias se les cobra la tarifa normal
         $tarjetaParcial = new FranquiciaParcial();
         $tarjetaGratuita = new FranquiciaCompleta();
 
-        $this->assertEquals($colectivoInterurbano->obtenerTarifa(), $tarjetaParcial->tarifaAPagar($colectivoInterurbano->obtenerTarifa()));
-        $this->assertEquals($colectivoInterurbano->obtenerTarifa(), $tarjetaGratuita->tarifaAPagar($colectivoInterurbano->obtenerTarifa()));
+        // Configurando un tiempo falso para simular el tiempo actual
+        $tiempoFalso = strtotime('2023-10-09 23:00:00');  
+        $tiempoOriginal = time();
+        $this->setTime($tiempoFalso);
+
+        $this->assertEquals($colectivo->obtenerTarifa(), $tarjetaParcial->tarifaAPagar($colectivo->obtenerTarifa()));
+        $this->assertEquals($colectivo->obtenerTarifa(), $tarjetaGratuita->tarifaAPagar($colectivo->obtenerTarifa()));
+ 
+        $this->setTime($tiempoOriginal);
     }
 }
